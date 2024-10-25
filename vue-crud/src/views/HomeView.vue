983fadcf2c92e1fad5534457ace9ac841d25fecd
Sorachat <sorachat.su@gmail.com>
2024-10-25 12:35:18 +0700

    EditHomeForCreate&Edit

--- vue-crud/src/views/HomeView.vue
+++ vue-crud/src/views/HomeView.vue
@@ -8,7 +8,7 @@
       title="Products"
       :rows="productRows"
       :columns="productColumns"
-      row-key="id"
+      row-key="product_id"
     >
       <template v-slot:body-cell-actions="props">
         <q-td :props="props">
@@ -18,11 +18,88 @@
       </template>
     </q-table>
 
+    <!-- Dialog for adding a new product -->
+    <q-dialog v-model="addProductDialog" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Create Product</div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="form-group">
+            <label for="productname">Product Name:</label>
+            <q-input v-model="productname" id="productname" required />
+          </div>
+
+          <div class="form-group">
+            <label for="stock">Stock:</label>
+            <q-input v-model="stock" type="number" id="stock" required min="1" />
+          </div>
+
+          <div class="form-group">
+            <label for="image">Image:</label>
+            <input type="file" @change="onFileChange" accept="image/jpeg, image/png, image/gif" required />
+          </div>
+
+          <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
+          <div v-if="successMessage" class="success">{{ successMessage }}</div>
+        </q-card-section>
+
+        <q-card-actions>
+          <q-btn color="primary" label="Create Product" @click="submitForm" />
+          <q-btn flat label="Cancel" @click="closeAddProductDialog" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- Dialog for editing a product -->
+    <q-dialog v-model="editProductDialog" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Edit Product</div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="form-group">
+            <label for="product_id">Product ID:</label>
+            <q-input v-model="currentProductId" id="product_id" readonly />
+          </div>
+
+          <div class="form-group">
+            <label for="productname">Product Name:</label>
+            <q-input v-model="productname" id="productname" required />
+          </div>
+
+          <div class="form-group">
+            <label for="stock">Stock:</label>
+            <q-input v-model="stock" type="number" id="stock" required min="1" />
+          </div>
+
+          <div class="form-group">
+            <label for="image">Image:</label>
+            <input type="file" @change="onEditFileChange" accept="image/jpeg, image/png, image/gif" />
+          </div>
+
+          <div v-if="editerrorMessage" class="error">{{ editerrorMessage }}</div>
+          <div v-if="editsuccessMessage" class="success">{{ editsuccessMessage }}</div>
+        </q-card-section>
+
+        <q-card-actions>
+          <q-btn color="primary" label="Edit Product" @click="submitEditForm(currentProductId)" />
+          <q-btn flat label="Cancel" @click="closeEditProductDialog" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- //---------------------------------------------------User Section-------------------------------------// -->
+    <!-- Add User Button -->
     <div class="q-py-md">
       <q-btn icon="add" @click="openAddUserDialog" label="Add User" />
     </div>
+
+    <!-- User Table -->
     <q-table
-      title="User"
+      title="Users"
       :rows="UserRows"
       :columns="UserColumns"
       row-key="user_id"
@@ -58,83 +135,28 @@
             <q-input v-model="email" id="email" required />
           </div>
 
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <q-input type="password" v-model="password" id="password" required />
+          </div>
 
-          <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
-          <div v-if="successMessage" class="success">{{ successMessage }}</div>
+          <div class="form-group">
+            <label for="role">Role:</label>
+            <q-select 
+              v-model="role" 
+              id="role" 
+              :options="roles" 
+              required 
+            />
+          </div>
+
+          <div v-if="usererrorMessage" class="error">{{ usererrorMessage }}</div>
+          <div v-if="usersuccessMessage" class="success">{{ usersuccessMessage }}</div>
         </q-card-section>
 
         <q-card-actions>
-          <q-btn color="primary" label="Create User" @click="submitForm" />
+          <q-btn color="primary" label="Create User" @click="submitUserForm" />
           <q-btn flat label="Cancel" @click="closeAddUserDialog" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-     <!-- Dialog for adding a new product -->
-     <q-dialog v-model="addProductDialog" persistent>
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Create Product</div>
-        </q-card-section>
-
-        <q-card-section>
-          <div class="form-group">
-            <label for="productname">Product Name:</label>
-            <q-input v-model="productname" id="productname" required />
-          </div>
-
-          <div class="form-group">
-            <label for="stock">Stock:</label>
-            <q-input v-model="stock" type="number" id="stock" required min="1" />
-          </div>
-
-          <div class="form-group">
-            <label for="image">Image:</label>
-            <input type="file" @change="onFileChange" accept="image/jpeg, image/png, image/gif" required />
-          </div>
-
-          <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
-          <div v-if="successMessage" class="success">{{ successMessage }}</div>
-        </q-card-section>
-
-        <q-card-actions>
-          <q-btn color="primary" label="Create Product" @click="submitForm" />
-          <q-btn flat label="Cancel" @click="closeAddProductDialog" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-
-    <!-- Dialog for edit a new product -->
-    <q-dialog v-model="editProductDialog" persistent>
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Edit Product</div>
-        </q-card-section>
-
-        <q-card-section>
-          <div class="form-group">
-            <label for="productname">Product Name:</label>
-            <q-input v-model="productname" id="productname" required />
-          </div>
-
-          <div class="form-group">
-            <label for="stock">Stock:</label>
-            <q-input v-model="stock" type="number" id="stock" required min="1" />
-          </div>
-
-          <div class="form-group">
-            <label for="image">Image:</label>
-            <input type="file" @change="onEditFileChange" accept="image/jpeg, image/png, image/gif" required />
-          </div>
-
-          <div v-if="editerrorMessage" class="error">{{ editerrorMessage }}</div>
-          <div v-if="editsuccessMessage" class="success">{{ editsuccessMessage }}</div>
-        </q-card-section>
-
-        <q-card-actions>
-          <q-btn color="primary" label="Edit Product" @click="submitEditForm" />
-          <q-btn flat label="Cancel" @click="closeEditProductDialog" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -144,8 +166,8 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
-import router from '@/router';
 
+// Define columns for the product table
 const productColumns = ref([
   { name: 'product_id', align: 'center', label: 'Product ID', field: 'product_id', sortable: true },
   { name: 'productname', align: 'left', label: 'Product Name', field: 'productname', sortable: true },
@@ -153,8 +175,10 @@ const productColumns = ref([
   { name: 'image', align: 'center', label: 'Image', field: 'image', sortable: true },
   { name: 'actions', align: 'center', label: 'Actions', field: 'actions', sortable: false },
 ]);
+
 const productRows = ref([]);
-// Fetch products
+
+// Fetch products from the backend
 const fetchProducts = () => {
   fetch('http://localhost:8800/api/v1/products')
     .then(res => res.json())
@@ -165,64 +189,67 @@ const fetchProducts = () => {
 };
 fetchProducts();
 
-// Dialog state and form data
+// Dialog state and form data for adding and editing
 const addProductDialog = ref(false);
+const editProductDialog = ref(false);
 const productname = ref('');
 const stock = ref(null);
 const image = ref(null);
+
 const errorMessage = ref('');
 const successMessage = ref('');
-
-// Dialog state and form data
-const editProductDialog = ref(false);
 const editerrorMessage = ref('');
 const editsuccessMessage = ref('');
 
-// Open the dialog
+const currentProductId = ref(null); // Stores the ID of the product being edited
+
+// Open the add product dialog
 const openAddProductDialog = () => {
-  productname.value = ''; // Reset the form
+  productname.value = '';
   stock.value = null;
   image.value = null;
-  editerrorMessage.value = '';
-  editsuccessMessage.value = '';
-  editProductDialog.value = true; // Open the dialog
+  errorMessage.value = '';
+  successMessage.value = '';
+  addProductDialog.value = true;
 };
 
-// Open the edit dialog
-const onEditProduct = (product_id) => {
-  // กำหนดค่า productname, stock และ image จากข้อมูลของสินค้าที่เลือกแก้ไข
-  const product = productRows.value.find(p => p.product_id === product_id);
-  productname.value = product.productname;
-  stock.value = product.stock;
-  image.value = null; // อาจจะต้องกำหนดวิธีการแสดงไฟล์ภาพในช่อง input ถ้าจำเป็น
-  editerrorMessage.value = '';
-  editsuccessMessage.value = '';
-  editProductDialog.value = true; // เปิด dialog สำหรับแก้ไขสินค้า
+// Open the edit product dialog and fetch product data by ID
+const onEditProduct = async (product_id) => {
+  try {
+    const response = await axios.get(`http://localhost:8800/api/v1/products/${product_id}`);
+    const product = response.data;
 
-  // ส่ง product_id ไปเพื่อใช้ใน submitEditForm
-  submitEditForm(product_id);
+    productname.value = product.productname;
+    stock.value = product.stock;
+    image.value = null;
+    
+    currentProductId.value = product_id; // Set currentProductId for editing
+    editerrorMessage.value = '';
+    editsuccessMessage.value = '';
+    editProductDialog.value = true; // Open edit dialog
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    editerrorMessage.value = 'Failed to fetch product data.';
+  }
 };
 
-// Close the dialog
-const closeAddProductDialog = () => {
-  addProductDialog.value = false; // Close the dialog
-};
-
-// Close the edit  dialog
-const closeEditProductDialog = () => {
-  editProductDialog.value = false; // Close the dialog
-};
-
-// Handle file change
+// Handle file changes for the add and edit forms
 const onFileChange = (event) => {
   image.value = event.target.files[0];
 };
-// Handle file change
 const onEditFileChange = (event) => {
   image.value = event.target.files[0];
 };
 
-// Submit form
+// Close the dialogs
+const closeAddProductDialog = () => {
+  addProductDialog.value = false;
+};
+const closeEditProductDialog = () => {
+  editProductDialog.value = false;
+};
+
+// Submit form for adding a new product
 const submitForm = async () => {
   errorMessage.value = '';
   successMessage.value = '';
@@ -239,9 +266,7 @@ const submitForm = async () => {
 
   try {
     const response = await axios.post('http://localhost:8800/api/v1/products', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
+      headers: { 'Content-Type': 'multipart/form-data' },
     });
     successMessage.value = response.data.message;
     fetchProducts(); // Refresh product data
@@ -255,54 +280,94 @@ const submitForm = async () => {
   }
 };
 
-// Submit form
+// Submit form for editing an existing product
 const submitEditForm = async (product_id) => {
   editerrorMessage.value = '';
   editsuccessMessage.value = '';
 
-  if (!productname.value || !stock.value || !image.value) {
-    errorMessage.value = 'All fields are required.';
+  if (!productname.value || !stock.value) {
+    editerrorMessage.value = 'Product name and stock are required.';
     return;
   }
 
-  const formData = new FormData();
-  formData.append('productname', productname.value);
-  formData.append('stock', parseInt(stock.value));
-  formData.append('image', image.value);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    productname: productname.value,
+    stock: parseInt(stock.value, 10),
+    // Handle image upload and get the URL if necessary
+    image: image.value ? await uploadImage(image.value) : null // Optional image upload function
+  });
+
+  const requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
 
   try {
-    const response = await axios.put(`http://localhost:8800/api/v1/products/${product_id}`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    editsuccessMessage.value = response.data.message;
-    fetchProducts(); // Refresh product data
-    closeEditProductDialog(); // Close the dialog
-  } catch (error) {
-    if (error.response && error.response.data) {
-      errorMessage.value = error.response.data.message || 'Failed to edit product.';
+    const response = await fetch(`http://localhost:8800/api/v1/products/${product_id}`, requestOptions);
+    const result = await response.json();
+
+    if (response.ok) {
+      alert('Update Success'); // Success alert
+      editsuccessMessage.value = result.message;
+      fetchProducts(); // Refresh product data
+      closeEditProductDialog(); // Close the dialog
     } else {
-      errorMessage.value = 'An unexpected error occurred.';
+      editerrorMessage.value = result.message || 'Failed to edit product.';
     }
+  } catch (error) {
+    console.error('Error updating product:', error);
+    editerrorMessage.value = 'An unexpected error occurred.';
   }
 };
 
-// const onEditProduct = (id) => {
-//   router.push(`/produpdate/${id}`);
-// };
+// Function to upload the image and return the URL
+const uploadImage = async (imageFile) => {
+  const formData = new FormData();
+  formData.append('image', imageFile);
 
+  const response = await fetch('http://localhost:8800/api/v1/upload', { // Adjust according to your upload endpoint
+    method: 'POST',
+    body: formData
+  });
+
+  const result = await response.json();
+  return result.imageUrl; // Return the uploaded image URL
+};
+
+// Delete a product
 const onDeleteProduct = (id) => {
   if (confirm("Are you sure you want to delete this product?")) {
     fetch(`http://localhost:8800/api/v1/products/${id}`, { method: 'DELETE' })
       .then(response => response.json())
       .then(result => {
         alert(result.message);
-        fetchProducts(); 
+        fetchProducts(); // Refresh product data
       })
       .catch(error => console.error('Error deleting product:', error));
   }
 };
+
+//script Users section
+
+// Define roles
+const roles = ref([
+  'Admin',
+  'User'
+]);
+// User Management
+const addUserDialog = ref(false); // Add user dialog
+const first_name = ref('');
+const last_name = ref('');
+const email = ref('');
+const password = ref('');
+const role = ref(''); // This will hold the string value of the selected role
+const usererrorMessage = ref('');
+const usersuccessMessage = ref('');
 
 
 const UserColumns = ref([
@@ -327,16 +392,75 @@ const fetchUser = () => {
 // Initial fetch
 fetchUser();
 
+// Delete a user
+const onDeleteCustomer = (id) => {
+  if (confirm("Are you sure you want to delete this User?")) {
+    fetch(`http://localhost:8800/api/v1/users/${id}`, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(result => {
+        alert(result.message);
+        fetchUser(); // Refresh user data
+      })
+      .catch(error => console.error('Error deleting User:', error));
+  }
+};
 
-// Dialog state and form data
-const addUserDialog = ref(false);
-const first_name = ref('');
-const last_name = ref(null);
-const email = ref(null);
-const usererrorMessage = ref('');
-const usersuccessMessage = ref('');
+// Function to open the add user dialog
+const openAddUserDialog = () => {
+  first_name.value = '';
+  last_name.value = '';
+  email.value = '';
+  password.value = '';
+  role.value = ''; // Reset role field
+  usererrorMessage.value = '';
+  usersuccessMessage.value = '';
+  addUserDialog.value = true; // Open the add user dialog
+};
+
+// Submit User Form
+const submitUserForm = async () => {
+  usererrorMessage.value = '';
+  usersuccessMessage.value = '';
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    first_name: first_name.value,
+    last_name: last_name.value,
+    email: email.value,
+    password: password.value, // Hash this password
+    role: role.value
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch("http://localhost:8800/api/v1/users", requestOptions);
+    const result = await response.json();
+    fetchUser();
+    closeAddUserDialog();
+    // Check for successful response
+    if (response.ok) {
+      alert(result.message); // Show success message
+    } else {
+      alert(result.message || "An error occurred. Please try again."); // Show error message
+    }
+  } catch (error) {
+    console.error('Error during signup:', error);
+    alert('An error occurred. Please try again later.'); // Generic error message
+  }
+};
 
 
+// Close Add User Dialog
+const closeAddUserDialog = () => {
+  addUserDialog.value = false; // Close the dialog
+};
 
 </script>
 
